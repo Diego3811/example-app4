ec20ea53e0477ff4a138255a9083ddc8ac16e1f5
CFiesta <zambrano_oscar450@hotmail.com>
2024-09-12 11:17:08 -0600

    se

--- app/Http/Controllers/Auth0Controller.php
+++ app/Http/Controllers/Auth0Controller.php
@@ -17,14 +17,27 @@ class Auth0Controller extends Controller
             clientId: 'vTiQ1nhWtg83UkYxE6SEyHo8K7rTA3rt',
             clientSecret: 'EUGNYuq3btrYIRqT5DCiww0lP-FKrHk1karUCsHRXx5vdgFOYauou8yKc9s9DneK',
             cookieSecret: Str::random(50),
+            redirectUri:'http:localhost:8000/callback'
         );
        $this-> $auth0 = new Auth0($configuration);
     }
 
     public function auth0(){
 
+        $session = $this->auth0->getCredentials();
+        if (null === $session || $session->accessTokenExpired) {
+        header('Location: ' . $auth0->login());
+        exit;
+}
+
     }
    public function callback(Request $request) {
-        
+    $input = $request->all();
+    if (null !== this->auth0->getExchangeParameters()) {
+        $auth0->exchange();
+    }
+    $user = $this->auth0->getCredentials()?->user;
+
+    dd($user);
     }
 }
